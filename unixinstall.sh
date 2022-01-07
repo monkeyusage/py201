5f6e0134790168c80379361303545208dd5c03b5
monkey usage <monkeyusage@gmail.com>
2022-01-07 15:12:20 +0100

    modify file structure, restore exercises

--- unixinstall.sh
+++ unixinstall.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 -m virtualenv venv && source venv/bin/activate && python -m pip install -r requirements.txt
+python3 -m pip install virtualenv && python3 -m virtualenv venv && source venv/bin/activate && python -m pip install -r requirements.txt
