5c66e9545db0ada61bc71356daa530e0cbf859dd
monkey usage <monkeyusage@gmail.com>
2022-01-25 14:19:04 +0100

    change sessions

--- unixinstall.sh
+++ unixinstall.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-python3 -m pip install virtualenv && virtualenv venv && source venv/bin/activate && python -m pip install -r requirements.txt
+python3 -m pip install virtualenv &&
+virtualenv venv &&
+source venv/bin/activate &&
+python -m pip install -r requirements.txt
